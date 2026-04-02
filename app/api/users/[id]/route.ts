0fcf371a54ee6169919d8a9c2dbb2680cff10588
Ameen Sid <ameensid7@outlook.com>
2026-04-02 13:48:16 +0530

    Fixed Error while deployment

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -5,10 +5,11 @@ import prisma from "@/src/lib/prisma";
 
 export async function PATCH(
 	req: Request,
-	{ params }: { params: { id: string } }
+	{ params }: { params: Promise<{ id: string }> }
 ) {
 	try {
-		const id = parseInt(params.id);
+        const { id: paramId } = await params;
+		const id = parseInt(paramId);
 		if (isNaN(id)) {
 			return NextResponse.json({ error: "Invalid User ID" }, { status: 400 });
 		}
@@ -55,10 +56,11 @@ export async function PATCH(
 
 export async function DELETE(
 	_req: Request,
-	{ params }: { params: { id: string } }
+	{ params }: { params: Promise<{ id: string }> }
 ) {
 	try {
-		const id = parseInt(params.id);
+        const { id: paramId } = await params;
+		const id = parseInt(paramId);
 		if (isNaN(id)) {
 			return NextResponse.json({ error: "Invalid User ID" }, { status: 400 });
 		}
